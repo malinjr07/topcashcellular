c42b3d7a087e5485d2de184acc1595bf3eaac459
malinjr07 <malinjr07@gmail.com>
2021-08-18 16:32:59 +0600

    data added

--- src/dummy-data/data.js
+++ src/dummy-data/data.js
@@ -156,7 +156,7 @@ const steps = [
     }
 ];
 
-const whyUs=[
+const whyUs = [
     {
         id: '1',
         title: 'High Price',
@@ -177,12 +177,144 @@ const whyUs=[
 
 /* HomePAge Data Complete */
 
-const help=[
+const help = [
     {
-        
+        id: '1',
+        question: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Aut vero, maxime itaque' +
+                ' provident quaerat nesciunt?',
+        answer: 'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quasi fuga consectetur' +
+                ' quas rem porro recusandae illum culpa minima atque reiciendis praesentium, aut ' +
+                'quibusdam expedita perferendis eaque, voluptatibus quo repellat iste!'
+    }, {
+        id: '2',
+        question: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Aut vero, maxime itaque' +
+                ' provident quaerat nesciunt?',
+        answer: 'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quasi fuga consectetur' +
+                ' quas rem porro recusandae illum culpa minima atque reiciendis praesentium, aut ' +
+                'quibusdam expedita perferendis eaque, voluptatibus quo repellat iste!'
+    }, {
+        id: '3',
+        question: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Aut vero, maxime itaque' +
+                ' provident quaerat nesciunt?',
+        answer: 'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quasi fuga consectetur' +
+                ' quas rem porro recusandae illum culpa minima atque reiciendis praesentium, aut ' +
+                'quibusdam expedita perferendis eaque, voluptatibus quo repellat iste!'
+    }, {
+        id: '4',
+        question: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Aut vero, maxime itaque' +
+                ' provident quaerat nesciunt?',
+        answer: 'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quasi fuga consectetur' +
+                ' quas rem porro recusandae illum culpa minima atque reiciendis praesentium, aut ' +
+                'quibusdam expedita perferendis eaque, voluptatibus quo repellat iste!'
+    }, {
+        id: '5',
+        question: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Aut vero, maxime itaque' +
+                ' provident quaerat nesciunt?',
+        answer: 'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quasi fuga consectetur' +
+                ' quas rem porro recusandae illum culpa minima atque reiciendis praesentium, aut ' +
+                'quibusdam expedita perferendis eaque, voluptatibus quo repellat iste!'
+    }
+];
+
+const user = [
+    {
+        id: '1',
+        name: 'Mosaddik Ali Maruf',
+        slug: 'malinjr07'
+    }, {
+        id: '2',
+        name: 'Mujahidul Islam',
+        slug: 'mujahid'
+    }, {
+        id: '3',
+        name: 'Elem Hasib',
+        slug: 'elems'
+    }
+];
+
+const orders = [
+    {
+        id: '1',
+        orderNo: 'dks2580ej',
+        orderStatus: 'pending',
+        amount: '550'
+    }, {
+        id: '1',
+        orderNo: 'dks285580ej',
+        orderStatus: 'shipped',
+        amount: '700'
+    }, {
+        id: '1',
+        orderNo: 'dks255680ej',
+        orderStatus: 'recieved',
+        amount: '1200'
+    }, {
+        id: '1',
+        orderNo: 'dks258087ej',
+        orderStatus: 'verifing',
+        amount: '990'
+    }, {
+        id: '1',
+        orderNo: 'dks258092ej',
+        orderStatus: 'completed',
+        amount: '1050'
+    }
+];
+
+const stock = [
+    {
+        id:'1',
+        device: 'samsung-s20-void',
+        orderNo: 'dks258092ej',
+        condition:'good',
+        storage: '256gb',
+        carrier: 'a&t'
     },
-]
+    {
+        id:'2',
+        device: 'samsung-s20-void',
+        orderNo: 'dks258087ej',
+        condition:'good',
+        storage: '256gb',
+        carrier: 't-mobile'
+    },
+    {
+        id:'3',
+        device: 'samsung-s20-void',
+        orderNo: 'dks255680ej',
+        condition:'good',
+        storage: '256gb',
+        carrier: 'sprint'
+    },
+    {
+        id:'4',
+        device: 'samsung-s20-void',
+        orderNo: 'dks285580ej',
+        condition:'good',
+        storage: '512gb',
+        carrier: 'varizon'
+    },
+    {
+        id:'5',
+        device: 'samsung-s20-void',
+        orderNo: 'dks2580ej',
+        condition:'good',
+        storage: '256gb',
+        carrier: 'unlocked'
+    },
+];
+
+
 
 export default link;
 
-export { category, products, steps, whyUs, help}
+export {
+    stock,
+    orders,
+    user,
+    category,
+    products,
+    steps,
+    whyUs,
+    help
+}
